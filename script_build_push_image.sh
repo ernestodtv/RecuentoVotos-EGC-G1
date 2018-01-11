4d0a622134bff7c314a171d27bebcad2dfbb049a
Javi <Trarsheise@gmail.com>
2018-01-12 00:39:22 +0100

    feat: Añade automatización de docker
    Añade a travis un script para reconstruir la imagen y subirla a docker hub.

---
+++ script_build_push_image.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export DOCKER_ID_USER=$DOCKER_USER
+docker login --username=$DOCKER_USER --password=$DOCKER_PASSWORD
+docker build -t $DOCKER_ID_USER/recvotes
+docker push $DOCKER_ID_USER/recvotes
