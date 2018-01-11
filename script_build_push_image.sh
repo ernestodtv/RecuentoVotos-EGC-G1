bf92f3030f1a3db25d393807e42363fb52ba48e8
Javi <Trarsheise@gmail.com>
2018-01-12 00:47:46 +0100

    bug: Añade un . que faltaba
    Añade . en el comando docker build en el script de automatizacion de docker para travis.

--- script_build_push_image.sh
+++ script_build_push_image.sh
@@ -2,5 +2,5 @@
 
 export DOCKER_ID_USER=$DOCKER_USER
 docker login --username=$DOCKER_USER --password=$DOCKER_PASSWORD
-docker build -t $DOCKER_ID_USER/recvotes
+docker build . -t $DOCKER_ID_USER/recvotes
 docker push $DOCKER_ID_USER/recvotes
