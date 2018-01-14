ec5b865c9d8b2e5a7cd0376876a566d6442f69ca
Unknown <javiihv93@gmail.com>
2018-01-14 14:07:12 +0100

    Añadido test de respuesta al servidor #27

--- recuento/tests.py
+++ recuento/tests.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+import unittest
+from django.test import TestCase, Client
 
-# Create your tests here.
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get('/recvotes/2/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the response is not 404.
+        self.assertNotEqual(response.status_code, 404)
+
+
